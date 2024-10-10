aa1bc59210cb2da87c97803d9522356b74a97527
Curpen Diyen <sollinselvan.curpen@umail.uom.ac.mu>
2024-10-10 14:25:23 +0400

    -Implement profile display with highlight -Add display of user details in the UI

--- src/user.c
+++ src/user.c
@@ -80,3 +80,40 @@ void saveProfileToFile(User user) {
 
   fclose(file);
 }
+
+void displayProfiles(WINDOW *win, User users[], int userCount, int highlight) {
+  for (int i = 0; i < userCount; i++) {
+    if (i == highlight) {
+      wattron(win, A_REVERSE); // Highlight the selected user
+    }
+    wattron(win, COLOR_PAIR(7)); // Set color for profile names
+    mvwprintw(win, 5 + i, 2, "%d. %s", i + 1,
+              users[i].name);     // Display user names starting at x = 2
+    wattroff(win, COLOR_PAIR(7)); // Turn off color for profile names
+    wattroff(win, A_REVERSE);     // Turn off the highlight
+  }
+
+  // Show the "Add New Profile" option at the bottom
+  if (userCount < MAX_USERS) {
+    if (userCount == highlight) {
+      wattron(win, A_REVERSE); // Highlight "Add New Profile" option
+    }
+    wattron(win, COLOR_PAIR(4)); // Set color for "Add New Profile"
+    mvwprintw(win, 5 + userCount, 2, "Add New Profile"); // Display the option
+    wattroff(win, COLOR_PAIR(4)); // Turn off color for "Add New Profile"
+    wattroff(win, A_REVERSE);     // Turn off the highlight
+  }
+
+  wattron(win, COLOR_PAIR(2)); // Color for navigation instruction text
+  mvwprintw(win, 5 + userCount + 2, 2,
+            "Use UP/DOWN to navigate, ENTER to select.");
+  wattroff(win,
+           COLOR_PAIR(2)); // Turn off color for navigation instruction text
+
+  // Redraw border after activity input
+  wattron(win, COLOR_PAIR(6)); // Turn on blue color
+  wborder(win, ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER,
+          ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
+  wattroff(win, COLOR_PAIR(6)); // Turn off blue color
+  wrefresh(win);                // Refresh the window after activity input
+}
